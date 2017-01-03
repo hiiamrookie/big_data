38c817cf669fbccf4688fb4960e25cc09f1b96f8
nimshmac025 <nimshmac025@nimshmac025deMacBook-Pro.local>
2017-01-03 11:07:03 +0800

    2017-01-03

---
+++ application/models/data_import_export.php
@@ -0,0 +1,30 @@
+<?php
+class Data_Import_Export extends User{
+	public function __construct(){
+		parent::__construct(FALSE,NULL);
+	}	
+	
+	public function get_data_import_html(){
+		$buf = file_get_contents(
+				TEMPLATE_PATH . 'report/data_import.tpl');
+		return str_replace(
+				array('[LEFT]', '[TOP]', '[VCODE]',
+						 '[BASE_URL]'),
+				array($this->get_left_html(), $this->get_top_html(),
+						$this->get_vcode(),
+						BASE_URL),
+				$buf);
+	}
+	
+	public function get_data_export_html(){
+		$buf = file_get_contents(
+				TEMPLATE_PATH . 'report/data_export.tpl');
+		return str_replace(
+				array('[LEFT]', '[TOP]', '[VCODE]',
+						'[BASE_URL]'),
+				array($this->get_left_html(), $this->get_top_html(),
+						$this->get_vcode(),
+						BASE_URL),
+				$buf);
+	}
+}
