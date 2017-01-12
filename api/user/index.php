47603ccc276393406140645a60012385adc2295a
nimshmac025 <nimshmac025@nimshmac025deMacBook-Pro.local>
2017-01-12 15:58:00 +0800

    2017-01-12 15:57

--- api/user/index.php
+++ api/user/index.php
@@ -4,15 +4,18 @@ include (dirname ( dirname ( dirname ( __FILE__ ) ) ) . '/inc/model_require.php'
 include (dirname ( dirname ( dirname ( __FILE__ ) ) ) . '/inc/require_file.php');
 header ( 'Content-type: text/html; charset=utf-8' );
 
+$o = Security_Util::my_get('o');
+//?o=media_schedule 上传媒体排期表
+
 $auth_name = Security_Util::my_post ( 'auth_name' );
 $auth_code = Security_Util::my_post ( 'auth_code' );
 $user_data = Security_Util::my_post ( 'user_data' );
 
+
+//$auth_name = 'test2';
+//$auth_code = '6b8ae6c537b02247ec2840f9445bce15';
+
 /*
-$auth_name = 'test2';
-$auth_code = '6b8ae6c537b02247ec2840f9445bce15';
-
-
 $a = array(
 		array(
 				'md5str'=>'dc7ec6d750ec265931329f879811992b',
@@ -34,8 +37,56 @@ $user_data = json_encode($a,JSON_UNESCAPED_UNICODE);
 */
 
 
+/*
+$a = array(
+		array(
+				'dsp_platform'=>'baidu',
+				'pid'=>'15SH010001-085',
+				'dsp_order'=>'订单X',
+				'dsp_adv'=>'广告X',
+				'dsp_creative'=>'创意X',
+				'dsp_website'=>'baidu.com',
+				'dsp_industry_1'=>'一级行业X',
+				'dsp_industry_2'=>'二级行业X',
+				'schedule_date'=>'2017-01-12',
+				'budget'=>array(
+						'0'=>100,
+						'11'=>200,
+						'22'=>300
+				),
+		),
+		array(
+				'dsp_platform'=>'baidu',
+				'pid'=>'15SH010001-085',
+				'dsp_order'=>'订单Y',
+				'dsp_adv'=>'广告Y',
+				'dsp_creative'=>'创意Y',
+				'dsp_website'=>'baidu.com',
+				'dsp_industry_1'=>'一级行业Y',
+				'dsp_industry_2'=>'二级行业Y',
+				'schedule_date'=>'2017-01-12',
+				'budget'=>array(
+						'10'=>1000,
+						'11'=>2000,
+						'12'=>3000,
+						'13'=>4000,
+						'14'=>5000,
+						'19'=>4000,
+						'20'=>3000,
+						'22'=>2000,
+						'23'=>1000,
+				),
+		)
+);
+
+$user_data = json_encode($a,JSON_UNESCAPED_UNICODE);
+*/
 
 
 $api = new Api_User_Data ( $auth_name, $auth_code );
-echo $api->getPostOfflineDataResult ( $user_data );
+if($o === 'media_schedule'){
+	echo $api->getPostMediaScheduleResult ( $user_data );
+}else{
+	echo $api->getPostOfflineDataResult ( $user_data );
+}
 unset ( $api );
--- application/models/api_user_data.php
+++ application/models/api_user_data.php
@@ -47,4 +47,73 @@ class Api_User_Data extends Api_Auth {
 				'message' => implode ( '，', $this->getAuthErrors () ) 
 		), JSON_UNESCAPED_UNICODE );
 	}
+	public function getPostMediaScheduleResult($user_data) {
+		if ($this->isAuthPass ()) {
+			if (! empty ( $user_data )) {
+				$user_data = json_decode ( $user_data );
+				if ($user_data !== NULL && $user_data !== FALSE) {
+					
+					$value_array = array ();
+					$budget_str = array ();
+					for($x = 0; $x < 24; $x ++) {
+						$budget_str [] = 'budget_' . $x;
+					}
+					
+					foreach ( $user_data as $ud ) {
+						$dsp_platform = $ud->dsp_platform;
+						$pid = $ud->pid;
+						$dsp_order = $ud->dsp_order;
+						$dsp_adv = $ud->dsp_adv;
+						$dsp_creative = $ud->dsp_creative;
+						$dsp_website = $ud->dsp_website;
+						$dsp_industry_1 = $ud->dsp_industry_1;
+						$dsp_industry_2 = $ud->dsp_industry_2;
+						$schedule_date = $ud->schedule_date;
+						$budgets = ( array ) $ud->budget;
+						
+						$budget_values = array(0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0);
+						$budget_sum = 0;
+						foreach ( $budgets as $times => $budget ) {
+							$budget_values [$times] = $budget;
+							$budget_sum += $budget;
+						}
+						
+						$url = str_replace ( array (
+								'http://',
+								'https://' 
+						), array (
+								'' 
+						), strtolower ( $dsp_website ) );
+						
+						$md5str = md5 ( $dsp_platform . '|' . $dsp_order . '|' . $dsp_adv . '|' . $dsp_creative . '|' . $url . '|' . $dsp_industry_1 . '|' . $dsp_industry_2 . '|' . $schedule_date );
+						$value_array [] = '("' . $dsp_platform . '","' . $pid . '","' . $dsp_order . '","' . $dsp_adv . '","' . $dsp_creative . '","' . $url . '","' . $dsp_industry_1 . '","' . $dsp_industry_2 . '","' . $schedule_date . '",' . implode ( ',', $budget_values ) . ',' . $budget_sum . ',now(),0,"' . $md5str . '")';
+					}
+					
+					if (! empty ( $value_array )) {
+						$result = $this->db->query ( 'INSERT INTO executive_media_schedule_content(dsp_platform,pid,dsp_order,dsp_adv,dsp_creative,dsp_website,dsp_industry_1,dsp_industry_2,schedule_date,' . implode ( ',', $budget_str ) . ',budget_sum,addtime,adduser,md5str) VALUES' . implode ( ',', $value_array ) );
+						return json_encode ( array (
+								'status' => $result === FALSE ? 'error' : 'success',
+								'message' => $result === FALSE ? $this->db->last_error : '传输数据成功' 
+						), JSON_UNESCAPED_UNICODE );
+					}
+					return json_encode ( array (
+							'status' => 'error',
+							'message' => '无数据' 
+					), JSON_UNESCAPED_UNICODE );
+				}
+				return json_encode ( array (
+						'status' => 'error',
+						'message' => '数据解析有误' 
+				), JSON_UNESCAPED_UNICODE );
+			}
+			return json_encode ( array (
+					'status' => 'error',
+					'message' => '数据不能为空' 
+			), JSON_UNESCAPED_UNICODE );
+		}
+		return json_encode ( array (
+				'status' => 'error',
+				'message' => implode ( '，', $this->getAuthErrors () ) 
+		), JSON_UNESCAPED_UNICODE );
+	}
 }
